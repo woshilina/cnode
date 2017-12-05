5b5e803ed6e25ee67a76498d3d6e108f3fd2c010
DESKTOP-3S03ITH\11780 <1178041164@qq.com>
2017-12-05 22:09:38 +0800

    优化

--- app.js
+++ app.js
@@ -28,17 +28,10 @@ const CONFIG = {
 app.use(session(CONFIG, app));
 
 app.use(serve('public'));
-// app.use(ctx => {
-//   // ignore favicon
-//   if (ctx.path === '/favicon.ico') return;
-//   let n = ctx.session.views || 0;
-//   ctx.session.views = ++n;
-//   ctx.body = n + ' views';
-// });
+
 app.use(require('./routers/onload').routes());
 app.use(require('./routers/signin').routes());
 app.use(require('./routers/main').routes());
- 
 app.use(require('./routers/topic').routes());
  
 app.listen(3000);
---
+++ controller/Login.js
@@ -0,0 +1,546 @@
+//引入数据库的模板
+const Topic = require('../database/models/topic');
+const Reply = require('../database/models/reply');
+const User = require('../database/models/user');
+const Message = require('../database/models/message');
+const moment = require('moment');
+const sequelize = require('../database/sequelize');
+moment().format();
+moment.locale('zh-cn');
+
+// 每页话题获取函数
+
+function page(page, AllCount, AllRows) {
+  var one = 20; //设置每页最多多少条数据
+  var AllRow = [];
+  if (AllCount >= one * page) {
+    for (var j = (page - 1) * one; j < one * page; j++) {
+      AllRow.push(AllRows[j]);
+    }
+  } else {
+    for (var j = (page - 1) * one; j < AllCount; j++) {
+      AllRow.push(AllRows[j]);
+    }
+  }
+  return AllRow;
+}
+
+// 登陆首页函数
+const homepage = async ctx => {
+  var tres = await Topic.findAll();
+
+  // 全部
+  for (var i of tres) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+  var result = await Topic.findAndCountAll({
+    where: {
+      $or: [
+        {
+          tabValue: '分享'
+        },
+        {
+          tabValue: '问答'
+        }
+      ]
+    },
+    order: [[sequelize.literal('lastreplytime DESC')]]
+  });
+
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+
+  // 积分榜
+  var users = await User.findAndCountAll({
+    order: [[sequelize.literal('integral DESC')]]
+  });
+  var rows = users.rows;
+  var usercount = users.count;
+  var tops = [];
+  if (usercount > 10) {
+    for (var i = 0; i < 10; i++) {
+      tops.push(rows[i]);
+    }
+  } else {
+    tops = rows;
+  }
+
+  var AllRows = result.rows;
+  var AllCount = result.count;
+  var AllRow = page(1, AllCount, AllRows);
+
+  // 无人回复的话题
+  var noreplytopics = await Topic.findAndCountAll({ where: { replies: 0 } });
+  var noreplycount = noreplytopics.count;
+  var allnoreplytopics = noreplytopics.rows;
+  var noreplytopics = [];
+  if (noreplycount > 5) {
+    for (var i = 0; i < 5; i++) {
+      noreplytopics.push(allnoreplytopics[i]);
+    }
+  } else {
+    noreplytopics = allnoreplytopics;
+  }
+  await ctx.render('/main', {
+    newcount: newcount,
+    session: ctx.session,
+    tops: tops,
+    usercount: usercount,
+    topics: AllRow,
+    noreplytopics: noreplytopics,
+    noreplycount: noreplycount
+  });
+};
+
+//设置首页分页按钮函数
+const homeallpage = async ctx => {
+  var tres = await Topic.findAll();
+  var one = 20; //设置每页最多多少条数据
+  // 全部
+  for (var i of tres) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+  var result = await Topic.findAndCountAll({
+    where: {
+      $or: [
+        {
+          tabValue: '分享'
+        },
+        {
+          tabValue: '问答'
+        }
+      ]
+    },
+    order: [[sequelize.literal('lastreplytime DESC')]]
+  });
+  var AllRow = result.rows;
+  var AllCount = result.count;
+  var totalpage =
+    AllCount % one == 0 ? AllCount / one : Math.ceil(AllCount / one);
+  ctx.body = {
+    total: totalpage
+  };
+};
+
+// 导航栏点击获取函数
+const hometabpage = async ctx => {
+  var tres = await Topic.findAll();
+  var p = ctx.params.page;
+  var AllCount;
+  var AllRows;
+  var AllRow = [];
+  var one = 20; //设置每页最多多少条数据
+  // 更新最后回复时间距现在多久
+
+  for (var i of tres) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+
+  // 点击全部时的显示
+
+  if (ctx.params.tab == 'all') {
+    let result = await Topic.findAndCountAll({
+      where: {
+        $or: [
+          {
+            tabValue: '分享'
+          },
+          {
+            tabValue: '问答'
+          }
+        ]
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  } else if (ctx.params.tab == 'essence') {
+    // 点击精华时的显示
+
+    let result = await Topic.findAndCountAll({
+      where: {
+        clicks: {
+          $gte: 50
+        }
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  } else if (ctx.params.tab == 'share') {
+    // 点击分享时的显示
+
+    let result = await Topic.findAndCountAll({
+      where: {
+        tabValue: '分享'
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  } else if (ctx.params.tab == 'ask') {
+    // 点击问答时的显示
+
+    let result = await Topic.findAndCountAll({
+      where: {
+        tabValue: '问答'
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  } else if (ctx.params.tab == 'job') {
+    // 点击招聘时的显示
+
+    let result = await Topic.findAndCountAll({
+      where: {
+        tabValue: '招聘'
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  } else if (ctx.params.tab == 'dev') {
+    // 点击客户端测试时的显示
+
+    let result = await Topic.findAndCountAll({
+      where: {
+        tabValue: '客户端测试'
+      },
+      order: [[sequelize.literal('lastreplytime DESC')]]
+    });
+    AllRows = result.rows;
+    AllCount = result.count;
+    AllRow = page(p, AllCount, AllRows);
+  }
+  var totalpage =
+    AllCount % one == 0 ? AllCount / one : Math.ceil(AllCount / one);
+  ctx.body = {
+    total: totalpage,
+    topics: AllRow
+  };
+};
+
+// get设置页函数
+const set = async ctx => {
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+  await ctx.render('/set', {
+    newcount: newcount,
+    session: ctx.session
+  });
+};
+
+//post设置个人信息函数
+const postset = async ctx => {
+  console.log(ctx.request.body);
+  var set = {
+    name: ctx.request.body.name,
+    Email: ctx.request.body.Email,
+    weibo: ctx.request.body.weibo,
+    web: ctx.request.body.web,
+    city: ctx.request.body.city,
+    sign: ctx.request.body.sign,
+    GitHub: ctx.request.body.GitHub
+  };
+  console.log(set);
+  var Id = ctx.session.id;
+  var user = await User.findById(Id);
+  await user.update(set);
+
+  // 更新session
+
+  ctx.session = {
+    id: Id,
+    name: user.name,
+    Email: user.Email,
+    weibo: user.weibo,
+    web: user.web,
+    city: user.city,
+    sign: user.sign,
+    GitHub: user.GitHub
+  };
+
+  console.log(ctx.session);
+
+  ctx.body = {
+    sign: set.sign
+  };
+};
+
+// 更改密码
+const setpass = async ctx => {
+  var pass = {
+    oldpass: ctx.request.body.oldpass,
+    newpass: ctx.request.body.newpass
+  };
+  var Id = ctx.session.id;
+  await User.findById(Id).then(user => {
+    console.log(user.password);
+    if (user.password === pass.oldpass) {
+      user.update({
+        password: pass.newpass
+      });
+      ctx.body = {
+        result: 'right'
+      };
+      console.log(ctx.body);
+    } else if (user.password != pass.oldpass) {
+      ctx.body = {
+        result: 'error'
+      };
+      console.log(ctx.body);
+    }
+  });
+};
+
+//个人主页
+const userpage = async ctx => {
+  var username = ctx.params.name;
+
+  // 从数据库获取用户信息
+  let user = await User.findOne({ where: { name: username } });
+  var signtime = user.createdAt;
+
+  // 更新用户注册时间
+  await user.update({ signfromnow: moment(signtime).fromNow() });
+
+  // 从数据库获取用户创建的话题 并按创建时间排序(时间倒序)
+  let result = await Topic.findAndCountAll({
+    where: { username: username },
+    order: [[sequelize.literal('createdAt DESC')]]
+  });
+  var count = result.count; //用户创建的话题数量
+  var topics = result.rows;
+
+  var pretopics = [];
+  if (count > 5) {
+    for (var i = 0; i < 5; i++) {
+      pretopics.push(topics[i]);
+    }
+  } else {
+    for (var i = 0; i < count; i++) {
+      pretopics.push(topics[i]);
+    }
+  }
+
+  // 更新最后回复时间
+  for (var i of topics) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+
+  // 从数据库获得用户的评论,按时间倒序
+  let reply = await Reply.findAll({
+    where: { name: username },
+    order: [[sequelize.literal('createdAt DESC')]]
+  });
+
+  // 根据用户评论获取相关话题
+  var parttopics = [];
+  for (var i of reply) {
+    var t = await Topic.findOne({ where: { id: i.topicId } });
+    parttopics.push(t);
+  }
+
+  // 数组去重
+  var hash = {};
+  parttopics = parttopics.reduce(function(item, next) {
+    hash[next.id] ? '' : (hash[next.id] = true && item.push(next));
+    return item;
+  }, []);
+
+  console.log(parttopics);
+  var partcount = parttopics.length;
+
+  var preparttopics = [];
+
+  if (partcount > 5) {
+    for (var i = 0; i < 5; i++) {
+      preparttopics.push(parttopics[i]);
+    }
+  } else {
+    for (var i = 0; i < partcount; i++) {
+      preparttopics.push(parttopics[i]);
+    }
+  }
+  console.log(preparttopics);
+
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+
+  await ctx.render('/spage', {
+    session: ctx.session,
+    user: user,
+    newcount: newcount,
+    pretopics: pretopics,
+    topics: topics,
+    count: count,
+    partcount: partcount,
+    preparttopics: preparttopics,
+    parttopics: parttopics
+  });
+};
+
+//// 个人创建话题页函数
+const createtopic = async ctx => {
+  var username = ctx.params.name;
+  let user = await User.findOne({ where: { name: username } });
+  // 从数据库获取用户创建的话题 并按创建时间排序(时间倒序)
+  let result = await Topic.findAndCountAll({
+    where: { username: username },
+    order: [[sequelize.literal('createdAt DESC')]]
+  });
+  var count = result.count;
+  var topics = result.rows;
+
+  // 更新最后回复时间
+  for (var i of topics) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+
+  await ctx.render('/spagetopics', {
+    session: ctx.session,
+    newcount: newcount,
+    user: user,
+    topics: topics
+  });
+};
+
+// 个人参与话题页函数
+const replytopic = async ctx => {
+  var username = ctx.params.name;
+  let user = await User.findOne({ where: { name: username } });
+  // 从数据库获得用户的评论,按时间倒序
+  let reply = await Reply.findAll({
+    where: { name: username },
+    order: [[sequelize.literal('createdAt DESC')]]
+  });
+
+  // 根据用户评论获取相关话题
+  var parttopics = [];
+  for (var i of reply) {
+    var t = await Topic.findOne({ where: { id: i.topicId } });
+    parttopics.push(t);
+  }
+  // 数组去重
+  var hash = {};
+  parttopics = parttopics.reduce(function(item, next) {
+    hash[next.id] ? '' : (hash[next.id] = true && item.push(next));
+    return item;
+  }, []);
+
+  // 更新最后回复时间
+  for (var i of parttopics) {
+    var time = i.lastreplytime;
+    await i.update({
+      lastreplyfromnow: moment(time).fromNow()
+    });
+  }
+
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+
+  await ctx.render('/spagereplies', {
+    session: ctx.session,
+    user: user,
+    newcount: newcount,
+    parttopics: parttopics
+  });
+};
+
+//积分榜页函数
+const usertop = async ctx => {
+  //查询未读消息数量
+  if (ctx.session.id) {
+    let news = await Message.findAndCountAll({
+      where: { targetId: ctx.session.id, hasRead: 0 }
+    });
+    var newcount = news.count;
+  }
+
+  // 积分榜
+  let users = await User.findAndCountAll({
+    order: [[sequelize.literal('integral DESC')]]
+  });
+  let rows = users.rows;
+  let usercount = users.count;
+  let topcount;
+  let tops = [];
+  if (usercount > 100) {
+    for (var i = 0; i < 100; i++) {
+      tops.push(rows[i]);
+    }
+    topcount = 100;
+  } else {
+    tops = rows;
+    topcount = usercount;
+  }
+
+  await ctx.render('./top', {
+    session: ctx.session,
+    newcount: newcount,
+    topcount: topcount,
+    tops: tops
+  });
+};
+
+module.exports = {
+  homepage,
+  homeallpage,
+  hometabpage,
+  set,
+  postset,
+  setpass,
+  userpage,
+  createtopic,
+  replytopic,
+  usertop
+};
