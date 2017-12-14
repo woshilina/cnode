319002957306ec785df6da9a4d5176ec559ceb5d
DESKTOP-3S03ITH\11780 <1178041164@qq.com>
2017-12-14 16:08:07 +0800

    更改数据库

--- database/config.js
+++ database/config.js
@@ -1,8 +1,8 @@
 var config = {
-  database: 'tjh59715fmtecsiz',
-  username: 'i5r80f0zmki4rl6p',
-  password: 'vkv4iq37wa1qy5yi',
-  host: 'ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  database: 'test',
+  username: 'root',
+  password: 'root',
+  host: 'localhost',
   port: 3306
 };
 
