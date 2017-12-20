8b8cd704b8d68e43ab08ba45a6339cf16c617b12
DESKTOP-3S03ITH\11780 <1178041164@qq.com>
2017-12-20 16:24:54 +0800

    Demo

--- database/config.js
+++ database/config.js
@@ -1,9 +1,15 @@
+// var config = {
+//   database: 'test',
+//   username: 'root',
+//   password: 'root',
+//   host: 'localhost',
+//   port: 3306
+// };
 var config = {
-  database: 'test',
-  username: 'root',
-  password: 'root',
-  host: 'localhost',
+  database: 'tjh59715fmtecsiz',
+  username: 'i5r80f0zmki4rl6p',
+  password: 'vkv4iq37wa1qy5yi',
+  host: 'ipobfcpvprjpmdo9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   port: 3306
 };
-
 module.exports = config;
